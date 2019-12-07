87cbc8e99ab4107933aa5bc84a77a9f36eb18e72
mikhail2013ru <mikhail2013ru@gmail.com>
2019-12-07 12:42:19 +0200

    fashion - старт проекта
    fashion - старт проекта

--- app/libs/js__src/common.js
+++ app/libs/js__src/common.js
@@ -1,57 +1,16 @@
-//Slider
+$('.carousel').carousel({
+    interval: 2000
+})
 
-//slideIndex - слайд в текущий момент
-let slideIndex = 1,
-    slides = document.querySelectorAll('.slider-item'),
-    prev = document.querySelector('.prev'),
-    next = document.querySelector('.next'),
-    dotsWrap = document.querySelector('.slider-dots'),
-    dots = document.querySelectorAll('.dot');
+//let link = document.querySelectorAll('.nav .list__link');
+let discount = document.getElementById('discount');
+let button = document.querySelector('.button1');
 
-    console.log(prev);
+console.log(button);
 
-showSlides(slideIndex);
-
-function showSlides(n) {
-
-    if(n < 1) {
-        slideIndex = slides.length;
-    } else if (n > slides.length) {
-        slideIndex = 1;
-    }
-    
-    slides.forEach((item) => item.style.display = 'none');
-    // for (let i = 0; i < slides.length; i++) {
-    //     slides[i].style.display = 'none';
-    // }
-
-    slideIndex++;
-    if (slideIndex > slides.length) { 
-       slideIndex = 1;
-    }
-
-    slides[slideIndex - 1].style.display = 'block';
-    setTimeout(showSlides, 3000);
-    //dots[slideIndex-1].className+= " active";
-}
-
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function minusSlides(n) {
-    showSlides(slideIndex -= n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-prev.addEventListener('click', function() {
-    plusSlides(-1);
+button.addEventListener('click', function(event) {
+    discount.scrollIntoView({
+        'behavior': 'smooth',
+    });
 });
-
-next.addEventListener('click', function() {
-    plusSlides(1);
-});
+//console.log(link[0]);
